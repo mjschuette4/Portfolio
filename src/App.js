d4ba86b15a985a805f5d633be62de1a57d8d742b
unknown <mjschuette4@gmail.com>
2020-12-07 16:21:17 -0600

    added icons to header for facebook, linkedin, twitter, github

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="app_header">
-        <Button>button 1</Button>
+      <Button>Blog</Button>
+        <div className="app_header_social">
+          <Button><GitHubIcon/></Button>
+          <Button><LinkedInIcon/></Button>
+          <Button><TwitterIcon/></Button>
+          <Button><FacebookIcon/></Button>
+        </div>
       </div>
       <div className="app_intro">
         <div className="app_content">
