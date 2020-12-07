5b894c001190ffa49569cf4eb3e1e2b3b242e91b
unknown <mjschuette4@gmail.com>
2020-12-07 15:05:32 -0600

    Finished landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import react from 'react';
+import React from 'react';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="app_content">
+        <h1>Hi, I'm Joe!</h1>
+      </div>
+      <div className="app_button">
+        <Button variant="contained" color="primary">Theme 1</Button>
+        <Button variant="contained" color="secondary">Theme 2</Button>
+      </div>
     </div>
   );
 }
