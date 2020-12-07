82222cb1519cb930a127476055f6d573c64663c9
unknown <mjschuette4@gmail.com>
2020-12-07 15:30:38 -0600

    Restructured landing page for nav bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="app_content">
-        <h1>Hi, I'm Joe!</h1>
+      <div className="app_header">
+
       </div>
-      <div className="app_button">
-        <Button variant="contained" color="primary">Theme 1</Button>
-        <Button variant="contained" color="secondary">Theme 2</Button>
+      <div className="app_intro">
+        <div className="app_content">
+          <h1>Hi, I'm Joe!</h1>
+        </div>
+        <div className="app_button">
+          <Button variant="contained" color="primary">Theme 1</Button>
+          <Button variant="contained" color="secondary">Theme 2</Button>
+        </div>
       </div>
     </div>
   );
